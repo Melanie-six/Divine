9e8370aec0c197ca6db6c4cf186e628bcadb42d6
Melanie <qwtyopei@gmail.com>
2026-02-25 15:49:19 +0800

    修改Header及Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,19 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
 function Footer() {
     return (
-        <footer>
-            this is footer
+        <footer className="bg-light py-2">
+            <div className="container">
+                <h3 className="text-center">Div.ine</h3>
+                <div className="d-flex justify-content-center">
+                    <i className="bi bi-facebook mx-2"></i>
+                    <i className="bi bi-instagram mx-2"></i>
+                    <i className="bi bi-threads mx-2"></i>
+                    <i className="bi bi-envelope mx-2"></i>
+                </div>
+                <p className="text-center fs-6">© 2024 Divine. All rights reserved.</p>
+            </div>
         </footer>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,28 @@
 import { Link } from "react-router";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 
 function Header () {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                    <Link className="nav-link" to="about">About</Link>
-                    <Link className="nav-link" to="products">Products</Link>
-                    <Link className="nav-link" to="cart">Cart</Link>
+            <div className="container">
+                <div className="m-2">
+                    <Link className="navbar-brand" to="/">Divine</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
                 </div>
+                <div className="collapse navbar-collapse d-lg-flex justify-content-between" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                        <Link className="nav-link" to="about">About</Link>
+                        <Link className="nav-link" to="products">Products</Link>
+                    </div>
+                    <div className="navbar-nav">
+                        <Link className="nav-link mx-2" to="cart"><i class="bi bi-cart3"></i></Link>
+                        <Link className="nav-link mx-3" to="login"><i class="bi bi-person-circle"></i></Link>
+                    </div>
                 </div>
             </div>
         </nav>
